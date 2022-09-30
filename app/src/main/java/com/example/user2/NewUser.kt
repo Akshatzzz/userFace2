c66ec46382fc498e953ff0d2d56c2a99ecc78e8a
Akshat <akshatchaturvedi99@gmail.com>
2022-09-30 10:36:12 +0530

    Implemetation using intent ACTION_PICK

--- app/src/main/java/com/example/user2/NewUser.kt
+++ app/src/main/java/com/example/user2/NewUser.kt
@@ -78,20 +78,16 @@ class NewUser : AppCompatActivity() {
 
 
         imageToBeLoaded.setOnClickListener {
-//            val pictureDialog = AlertDialog.Builder(this)
-//            pictureDialog.setTitle("Select Action")
-//            val pictureDialogItem = arrayOf("Open Gallery", "Open Camera")
-//            pictureDialog.setItems(pictureDialogItem) { dialog, which ->
-//                when (which) {
-//                    0 -> galleryCheckPermission()
-//                    1 -> cameraCheckPermission()
-//                }
-//            }
-//            pictureDialog.show()
-            ImagePicker.with(this)
-                //...
-                .provider(ImageProvider.BOTH) //Or bothCameraGallery()
-                .createIntentFromDialog { launcher.launch(it) }
+            val pictureDialog = AlertDialog.Builder(this)
+            pictureDialog.setTitle("Select Action")
+            val pictureDialogItem = arrayOf("Open Gallery", "Open Camera")
+            pictureDialog.setItems(pictureDialogItem) { dialog, which ->
+                when (which) {
+                    0 -> galleryCheckPermission()
+                    1 -> cameraCheckPermission()
+                }
+            }
+            pictureDialog.show()
 
 
         }
@@ -132,9 +128,8 @@ class NewUser : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == Activity.RESULT_OK) {
                 val uri = it.data?.data!!
-                bit = Images.Media.getBitmap(contentResolver, uri)
-                val tempUri: Uri = getImageUriFromBitmap(applicationContext, bit!!)
-                imageUri = tempUri
+                imageUri = uri
+
                 Glide.with(this)
                     .load(imageUri)
                     .into(imageToBeLoaded)
@@ -144,115 +139,110 @@ class NewUser : AppCompatActivity() {
             }
         }
 
-//    private fun cameraCheckPermission() {
-//        Dexter.withContext(applicationContext)
-//            .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
-//            .withListener(
-//                object : MultiplePermissionsListener {
-//                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-//                        report?.let {
-//                            if (report.areAllPermissionsGranted()) {
-//                                launcher.launch(
-//                                    ImagePicker.with(this@NewUser)
-//                                        //...
-//                                        .cameraOnly() // or galleryOnly()
-//                                        .createIntent()
-//                                )
+    private fun cameraCheckPermission() {
+        Dexter.withContext(applicationContext)
+            .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+            .withListener(
+                object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        report?.let {
+                            if (report.areAllPermissionsGranted()) {
+                                launcher.launch(
+                                    ImagePicker.with(this@NewUser)
+                                        //...
+                                        .cameraOnly() // or galleryOnly()
+                                        .createIntent()
+                                )
+
+                            }
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        p0: MutableList<PermissionRequest>?,
+                        p1: PermissionToken?
+                    ) {
+                        showRotationalDialogForPermission()
+                    }
+                }
+            ).onSameThread().check()
+    }
+
+
+    private fun galleryCheckPermission() {
+        Dexter.withContext(applicationContext).withPermission(
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ).withListener(object : PermissionListener {
+            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+//                contract.launch(arrayOf("image/*"))
+                gallery()
+            }
+
+            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                Toast.makeText(this@NewUser, "Storage Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                p0: PermissionRequest?,
+                p1: PermissionToken?
+            ) {
+                showRotationalDialogForPermission()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun gallery() {
+        intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+
+    }
+
+
+    private val contract = registerForActivityResult(ActivityResultContracts.OpenDocument())
+    {
+        imageUri = it
+//        imageToBeLoaded.setImageURI(it)
+        Glide.with(this)
+            .load(it)
+            .into(imageToBeLoaded)
+
+        Log.d("URIXYZ","$imageUri")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+//                CAMERA_REQUEST_CODE -> {
+//                    bit = data?.extras?.get("data") as Bitmap
+//                    imageToBeLoaded.setImageBitmap(bit)
 //
-//                            }
-//                        }
-//                    }
-//
-//                    override fun onPermissionRationaleShouldBeShown(
-//                        p0: MutableList<PermissionRequest>?,
-//                        p1: PermissionToken?
-//                    ) {
-//                        showRotationalDialogForPermission()
-//                    }
+//                    val tempUri: Uri? = getImageUriFromBitmap(applicationContext, bit!!)
+////                    val finalFile:File = File(getRealPathFromURI(tempUri))
+//                    imageUri = tempUri
+//                    Glide.with(this)
+//                        .load(imageUri)
+//                        .into(imageToBeLoaded)
 //                }
-//            ).onSameThread().check()
-//    }
-//
-//
-//    private fun galleryCheckPermission() {
-//        Dexter.withContext(applicationContext).withPermission(
-//            Manifest.permission.READ_EXTERNAL_STORAGE
-//        ).withListener(object : PermissionListener {
-//            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-////                contract.launch(arrayOf("image/*"))
-//                gallery()
-//            }
-//
-//            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
-//                Toast.makeText(this@NewUser, "Storage Permission Denied", Toast.LENGTH_SHORT).show()
-//            }
-//
-//            override fun onPermissionRationaleShouldBeShown(
-//                p0: PermissionRequest?,
-//                p1: PermissionToken?
-//            ) {
-//                showRotationalDialogForPermission()
-//            }
-//        }).onSameThread().check()
-//    }
-//
-//    private fun gallery() {
-////        intent = Intent(Intent.ACTION_PICK)
-////        intent.type = "image/*"
-////        startActivityForResult(intent, GALLERY_REQUEST_CODE)
-//        ImagePicker.with(this@NewUser)
-//            .galleryOnly()  //User can only select image from Gallery
-//            .createIntent()
-//
-//
-//    }
-//
-//
-////    private val contract = registerForActivityResult(ActivityResultContracts.OpenDocument())
-////    {
-////        imageUri = it
-//////        imageToBeLoaded.setImageURI(it)
-////        Glide.with(this)
-////            .load(it)
-////            .into(imageToBeLoaded)
-////
-////        Log.d("URIXYZ","$imageUri")
-////    }
-//
-////    @Deprecated("Deprecated in Java")
-////    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-////        super.onActivityResult(requestCode, resultCode, data)
-////
-////        if (resultCode == RESULT_OK) {
-////            when (requestCode) {
-//////                CAMERA_REQUEST_CODE -> {
-//////                    bit = data?.extras?.get("data") as Bitmap
-//////                    imageToBeLoaded.setImageBitmap(bit)
-//////
-//////                    val tempUri: Uri? = getImageUriFromBitmap(applicationContext, bit!!)
-////////                    val finalFile:File = File(getRealPathFromURI(tempUri))
-//////                    imageUri = tempUri
-//////                    Glide.with(this)
-//////                        .load(imageUri)
-//////                        .into(imageToBeLoaded)
-//////                }
-////
-////                GALLERY_REQUEST_CODE -> {
-////                    data!!.data.also { imageUri = it }
-////                    bit = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
-////                    val tempUri: Uri = getImageUriFromBitmap(applicationContext, bit!!)
-//////                    val finalFile:File = File(getRealPathFromURI(tempUri))
-//////                    imageUri = tempUri
-////
-////                    Glide.with(this)
-////                        .load(imageUri)
-////                        .into(imageToBeLoaded)
-//////                    imageToBeLoaded.setImageBitmap(bit)
-////
-////                }
-////            }
-////        }
-////    }
+
+                GALLERY_REQUEST_CODE -> {
+                    data!!.data.also { imageUri = it }
+                    bit = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+                    val tempUri: Uri = getImageUriFromBitmap(applicationContext, bit!!)
+//                    val finalFile:File = File(getRealPathFromURI(tempUri))
+                    imageUri = tempUri
+
+                    Glide.with(this)
+                        .load(imageUri)
+                        .into(imageToBeLoaded)
+//                    imageToBeLoaded.setImageBitmap(bit)
+
+                }
+            }
+        }
+    }
 
     private fun getImageUriFromBitmap(context: Context?, bitmap: Bitmap): Uri {
         val bytes = ByteArrayOutputStream()
@@ -262,32 +252,32 @@ class NewUser : AppCompatActivity() {
 
     }
 
-//    private fun showRotationalDialogForPermission() {
-//        AlertDialog.Builder(this)
-//            .setMessage(
-//                "It looks like you have turned off permissions"
-//                        + "required for this feature. It can be enable from App Settings"
-//            )
-//            .setPositiveButton(
-//                "Go to settings"
-//            ) { _, _ ->
-//                try {
-//                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-//                    val uri = Uri.fromParts("package", packageName, null)
-//                    intent.data = uri
-//                    startActivity(intent)
-//                } catch (e: ActivityNotFoundException) {
-//                    e.printStackTrace()
-//                }
-//            }
-//
-//
-//            .setNegativeButton(
-//                "Cancel"
-//            ) { dialog, _ ->
-//                dialog.dismiss()
-//            }.show()
-//    }
+    private fun showRotationalDialogForPermission() {
+        AlertDialog.Builder(this)
+            .setMessage(
+                "It looks like you have turned off permissions"
+                        + "required for this feature. It can be enable from App Settings"
+            )
+            .setPositiveButton(
+                "Go to settings"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+
+
+            .setNegativeButton(
+                "Cancel"
+            ) { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
 
 
     override fun onBackPressed() {
